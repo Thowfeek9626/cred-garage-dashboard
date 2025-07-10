3efe52a230f0b50d39d49430400e5df573d6fc0d
Thow-oma <mohamedthowfeek9626@gmail.com>
2025-07-10 14:00:24 +0400

    refactor: replace hardcoded user data with Firebase fetch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,34 @@
-import BenefitsGrid from "./components/BenefitsGrid";
+'use client';
+
 import RewardPoints from "./components/RewardPoints";
-import ThemeToggle from "./components/ThemeToggle";
+import { useFetchUserProfile } from "./hooks/useFetchUserProfile";
+import { useFetchRewards } from "./hooks/useFetchRewards";
 import UserProfile from "./components/UserProfile";
+import BenefitsGrid from "./components/BenefitsGrid";
+import DashboardFooter from "./components/Footer";
+import ThemeToggle from "./components/ThemeToggle";
 
 export default function Home() {
+  const userId = 'F0E2ZvHF75uuo2BlokZu';
+  const { profile, loading } = useFetchUserProfile(userId);
+  const { reward, loading: rewardLoading } = useFetchRewards('RJz9ZtjgO96FNCPy9M6H');
+
   return (
-    <main className="max-w-5xl mx-auto px-4 py-8">
-      <div className="flex justify-between items-center mb-6">
-        <h1 className="text-2xl font-bold text-zinc-800 dark:text-white">Dashboard</h1>
-        <ThemeToggle />
-      </div>
-      <UserProfile />
-      <RewardPoints />
-      <BenefitsGrid />
-    </main>
+    <div className="flex flex-col justify-between min-h-screen">
+      <header>
+        <div className="flex flex-col sm:flex-row justify-between items-center m-8 gap-4">
+          <h1 className="text-3xl font-extrabold text-zinc-900 dark:text-white tracking-tight">CRED Garage</h1>
+          <ThemeToggle />
+        </div>
+      </header>
+
+      <main className="max-w-6xl mx-auto px-4 py-10">
+        <UserProfile loading={loading} profile={profile} />
+        <RewardPoints loading={rewardLoading} reward={reward} />
+        <BenefitsGrid />
+      </main>
+
+      <DashboardFooter />
+    </div>
   );
 }
